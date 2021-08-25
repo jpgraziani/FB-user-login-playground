96c0bea1d40936d326cbbd0c90f818d6ef90ad26
Jonathan Graziani <jonathangraziani@Jonathans-MacBook-Air.local>
2021-08-25 15:14:22 -0700

    fb login

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Signup from './Signup'
+import Dashboard from './Dashboard'
+import Login from './Login'
+import { Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../context/AuthContext'
+
+const App = () => {
+  return (
+    <>
+      
+        <Container 
+          className='d-flex align-items-center justify-content-center'
+          style={{ minHeight: "100vh" }}
+        >
+          <div className='w-100' style={{ maxWidth: '400px' }}>
+            <AuthProvider>
+              <Route exact path='/' component={Dashboard} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/login' component={Login} />
+            </AuthProvider>
+          </div>
+        </Container>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+const PrivateRoute = ({ component: component, ...rest }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PrivateRoute
